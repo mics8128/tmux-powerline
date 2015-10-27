a71404c647a734ba132343654da8a12dd6fb7924
Mics <mics.mm@gmail.com>
2015-10-28 02:47:20 +0800

    add cygwin cpu load info

--- segments/load.sh
+++ segments/load.sh
@@ -1,7 +1,10 @@
 # Print the average load.
 
 run_segment() {
-    if !(shell_is_cygwin) ; then
+    if (shell_is_cygwin) ; then
+        cpu_useage=$(wmic cpu get loadpercentage | sed -n "2p" | xargs | sed -e "s/^[ \t]*//")
+        echo CPU: $cpu_useage%
+    else
         uptime | cut -d "," -f 3- | cut -d ":" -f2 | sed -e "s/^[ \t]*//"
     fi
 	exit 0
